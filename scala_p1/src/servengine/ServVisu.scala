4269f953f5bbd8366bd2d0752c5699e8d871bf43
DFI <dfi@dfidaali.com>
2015-09-27 02:31:49 +0200

    color ajustees

--- scala_p1/src/servengine/ServVisu.scala
+++ scala_p1/src/servengine/ServVisu.scala
@@ -24,7 +24,7 @@ object  ServVisu extends SimpleSwingApplication {
     preferredSize = new Dimension(600,400)
     val cz=20
 
-    val allCol= Array(Color.lightGray,Color.red,Color.green,Color.blue,Color.yellow)
+    val allCol= Array(Color.black,Color.pink,Color.green,Color.blue,Color.yellow)
     
     opaque = true
     background = Color.darkGray
@@ -37,10 +37,21 @@ object  ServVisu extends SimpleSwingApplication {
         val p=(model.officialMap.dat)(i)(j)+1
         g.setColor(allCol(p))
         
+        
         g.fillRect(i*cz, j*cz, cz-1, cz-1)
       }
       
-
+      var id=0
+      for(c <- model.rawTurn.head){
+          
+          id=id +1
+          val i=c.x
+          val j=c.y
+          g.setColor(Color.DARK_GRAY)
+          g.fillRect(i*cz +cz/4, j*cz + cz/4, cz/2, cz/2)
+          g.setColor(allCol(id))
+          g.fillRect(i*cz +cz/4+cz/6, j*cz +cz/4+cz/6, cz/3, cz/3)
+      }
       
     }  
   }

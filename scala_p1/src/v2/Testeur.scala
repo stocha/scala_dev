8921d3cc0b995c84522d04e1807389b475d0e16c
DFI <dfi@dfidaali.com>
2015-09-29 08:28:27 +0200

    sim 003

--- scala_p1/src/v2/Testeur.scala
+++ scala_p1/src/v2/Testeur.scala
@@ -20,12 +20,35 @@ object Testeur extends App {
       game= game.transition(n)
       System.err.println("Turn "+i);
       System.err.println(""+game);
-    }
+    }    
+  }
+  
+  def bench001{
+    val r = new Random(0x8377);
+    val randLong=Math.abs(r.nextLong())
+    var game=GameState4P.start(64564544557L)
+    
+    def nbBoucle=1000000;
+    
+    val t0 = System.nanoTime()
+    for(i <- 0 until nbBoucle){
+      val n = r.nextInt(GameState4P.moveMax)
+      game= game.transition(n)
+
+    }       
+    val t1 = System.nanoTime()
+    
+    val t : Double=(t1 - t0)
+    
+   System.err.println(""+game);        
+   System.err.println(" Per sec "+(nbBoucle.toDouble / (t/(1000*1000*1000)) ));
+
     
   }
   
    override def main(args: Array[String]) {
-    testGS001
+    //testGS001
+     bench001
   }
   
 }

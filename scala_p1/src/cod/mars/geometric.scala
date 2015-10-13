62b21f940c65592a31f7095cb462c9d433a8a9b1
DFI <dfi@dfidaali.com>
2015-10-13 12:55:19 +0200

    implicit rich conversion

--- scala_p1/src/cod/mars/geometric.scala
+++ scala_p1/src/cod/mars/geometric.scala
@@ -5,33 +5,49 @@ package cod.mars
  */
 object geometric extends App {
 
-  type StructCoord = { def getCoord(): point }
+  type StructCoord = { def getCoord : point }
 
   object point {
     def apply(dx: Double, dy: Double) = {
       new point(dx, dy)
     }
     
-    implicit def richPoint(x : StructCoord){
+    def normedY()={
+      point(0,1)
+    }
+    
+    def normedX()={
+      point(1,0)
+    }
+    
+    implicit def richPoint(x : StructCoord)={
       new pointManip(x)
     }
+    
+
   }
 
   class point(val x: Double, val y: Double) {
-
-  }
-
-  trait hasCoord {
-    def getCoord: point
+    override def toString={
+      val res="("+x+","+y+")"
+      res
+    }
+    
+    def getCoord : point={
+      this
+    }    
   }
 
   class pointManip(val p : StructCoord) {
-
+    def *( s : Double)={
+      point(p.getCoord.x*s,p.getCoord.y*s)
+    }
   }
   
   
    override def main(args: Array[String]) {
-     Console.err.println(" Hello there");
+     Console.err.println(" Hello there "+(point(4,6.5)*5.0));
+     Console.err.println(" Hello there "+(point.normedY()*5.0));
    
    }
 }

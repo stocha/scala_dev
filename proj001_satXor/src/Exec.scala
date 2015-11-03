3ac370ce2bb2553e93a665dec5d191cd8a4920ee
DFI <dfi@dfidaali.com>
2015-11-03 21:23:54 +0100

    line simples

--- proj001_satXor/src/Exec.scala
+++ proj001_satXor/src/Exec.scala
@@ -5,4 +5,10 @@
  */
 object Exec extends App {
   println( "coucou");
+  
+  val x= T()+ T(0,4) + T(4,0) + T();
+  val y= T(0,5) + T(1,4) + T(2,6);
+  
+  println(" "+x +"  "+y);
+  
 }
--- proj001_satXor/src/xorEquation.scala
+++ proj001_satXor/src/xorEquation.scala
@@ -3,36 +3,105 @@
 import scala.collection.immutable.Vector
 import scala.collection.immutable.TreeSet
 
+class conj(val it: TreeSet[Short]) extends Ordered[conj] {
 
-class conj extends Ordered[conj]{
-  val it=TreeSet[Short]();
-  
-  def compare( b:conj) : Int = {
-    -1
+  def compare(b: conj): Int = {
+
+    def doit(b: conj): Int = {
+      val sz = -b.it.size + it.size
+
+      def dothem(x: List[Int]): Int = {
+        if (x.isEmpty) 0 else if (x.head < 0) -1 else if (x.head > 0) 1
+        else dothem(x.tail);
+      }
+
+      if (sz < 0) -1 else if (sz > 0) 1 else {
+        val them = for ((m, a) <- (it zip b.it).toList) yield {
+          m - a
+        }
+        dothem(them);
+      }
     }
-  
+
+    val res = doit(b)
+    //  println("Comparing " + this + " and " + b +" -> "+res);
+
+    res
+
+  }
+  /* 
+  override def equals(o: Any) = o match {
+    
+    
+    case that: conj => compare(that)==0
+    case _ => false
+  }*/
+
+  def *(v: Short) = {
+    new conj(it + v)
+  }
+
+  def +(ot: conj) = {
+    new xor(new TreeSet[conj]) + this + ot;
+  }
+
+  override def toString = {
+
+    if (it.isEmpty) "T" else {
+
+      var res = "";
+      var has = false;
+      for (s <- it) {
+        if (has) res = res + ".";
+        res = res + s.toHexString
+        has = true;
+      }
+      res
+
+    }
+  }
+
 }
 
-class xor  extends Ordered[xor]{
-  val it=TreeSet[conj]();
-  
-  def compare( b:xor) : Int = {
+class xor(val it: TreeSet[conj]) extends Ordered[xor] {
+
+  def compare(b: xor): Int = {
     -1
-    }  
+  }
+
+  def +(v: conj) = {
+    if (it.contains(v)) new xor(it - v) else {
+      new xor(it + v)
+    }
+
+  }
+
+  override def toString = {
+    if (it.isEmpty) "F" else {
+
+      var res = "";
+      var has = false;
+      for (s <- it) {
+        if (has) res = res + " + ";
+        res = res + s
+        has = true;
+      }
+      res
+    }
+  }
 }
 
-class equation{
-   val it=TreeSet[xor]() ;
+class equation {
+  val it = TreeSet[xor]();
 }
 
 object T {
-  
-   def apply : conj ={
-    new conj;
+  def apply(vs: Short*): conj = {
+    var x = new conj(new TreeSet);
+
+    for (v <- vs) {
+      x = x * v
+    }
+    x
   }
-   
-   def apply(v : Short*) : conj ={
-    new conj;
-  }   
-  
 }
